6af2ed801a288e3c30f111ec649fa91e6d60685c
tejastej1997 <tejastej1997@gmail.com>
2023-07-09 21:52:54 +0530

    modified:   calculator.css 	modified:   calculator.js 	modified:   index.html

--- calculator.js
+++ calculator.js
@@ -1,1 +1,28 @@
-let day = document.getAnimations('day')
+let day = document.getElementById('day')
+
+let month = document.getElementById('month')
+let year = document.getElementById('year')
+let eday = document.getElementById('eday')
+let emonth = document.getElementById('emonth')
+let eyear = document.getElementById('eyear')
+
+
+let getage =()=>{
+
+    let currentdate = new Date()
+    let currentmonth=currentdate.getMonth()+1;
+    console.log(currentdate.getFullYear());
+
+    day.innerHTML=currentdate.getDate()-eday.value;
+    month.innerHTML=currentmonth-emonth.value;
+    year.innerHTML=currentdate.getFullYear()-eyear.value;
+    console.log('hello');
+}
+
+
+
+eyear.addEventListener('keyup',(e)=>{
+    if (e.key=='Enter') {
+        getage()
+    }
+})
